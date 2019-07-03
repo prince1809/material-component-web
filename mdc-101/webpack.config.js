f6d63a4d95a7810d15108c89575df3624bedd27d
prince <prince18pat@gmail.com>
2019-07-04 00:16:38 +0900

    add starter files

---
+++ mdc-101/webpack.config.js
@@ -0,0 +1,17 @@
+function getStyleUse(bundleFilename) {
+  return [
+    {
+
+    }
+  ]
+}
+
+module.exports = [
+  {
+    entry: './login.scss',
+    output: {
+      filename: 'style-bundle-login.js'
+    }
+  }
+
+];
